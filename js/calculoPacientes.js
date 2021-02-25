e0b00a5722c5e2421d214956da48d1dda31668e3
Nerea García <nereagarcia.c@gmail.com>
2021-02-25 09:48:18 +0100

    LImpieza funcion calculoPacientes

--- js/calculoPacientes.js
+++ js/calculoPacientes.js
@@ -4,23 +4,24 @@ function calculoPacientes(pacientes) {
     .filter(elemento => elemento.paciente.edad >= 18)
     .length;
   const nHombresDiabeticos = pacientes
-    .filter(elemento => elemento.dieta === "Diabetes")
-    .filter(elemento => elemento.paciente.sexo === "H")
+    .filter(elemento => elemento.dieta === "Diabetes"
+      && elemento.paciente.sexo === "H")
     .length;
   const totalDiasIngreso = pacientes
     .reduce((acc, elemento) => acc + elemento.diasIngresado, 0);
   const nMujeres = pacientes
     .filter(elemento => elemento.paciente.sexo === "M");
-  const mediaEdadMujeres = nMujeres
+  const mediaEdadMujeres0 = nMujeres
     .reduce((acc, elemento) => acc + elemento.paciente.edad, 0)
     / nMujeres.length;
+  const mediaEdadMujeres = mediaEdadMujeres0.toFixed(2);
 
   const pacientesCalculo = {
     nPacientes,
     nMayoresEdad,
     nHombresDiabeticos,
     totalDiasIngreso,
-    mediaEdadMujeres
+    mediaEdadMujeres,
   };
   return pacientesCalculo;
 }
